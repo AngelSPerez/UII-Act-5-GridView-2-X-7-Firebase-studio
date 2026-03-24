609657de85ed563ed096d701cdc70a753d3a48b9
a.23308051281160 <a.23308051281160@cbtis128.edu.mx>
2026-03-24 17:01:39 +0000

    ex2

--- lib/main.dart
+++ lib/main.dart
@@ -7,116 +7,223 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ReciveRecipe',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF8939EF),
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePageWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePageWidget> createState() => _HomePageWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageWidgetState extends State<HomePageWidget> {
+  final List<Map<String, String>> _recipes = [
+    {
+      'name': 'Boneless',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/boneless.webp',
+    },
+    {
+      'name': 'Pizza',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/pizza.jpg',
+    },
+    {
+      'name': 'Flan',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/Spanish-Desserts.jpeg',
+    },
+    {
+      'name': 'Pasta Alfredo',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/Fettuccine-Alfredo-Recipe-1.jpg',
+    },
+    {
+      'name': 'Pay de Manzana',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/maxresdefault.jpg',
+    },
+    {
+      'name': 'Sandwich',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/23132.jpg',
+    },
+    {
+      'name': 'Torta de Jamón',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/torta-de-jamon.jpg',
+    },
+    {
+      'name': 'Pollo Asado',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/Pollo-asado-Jordi-Cruz.jpg',
+    },
+    {
+      'name': 'Galletas',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/R.jpeg',
+    },
+    {
+      'name': 'Pay de Manzana',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/maxresdefault.jpg',
+    },
+    {
+      'name': 'Pay de Queso',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/OIP.webp',
+    },
+    {
+      'name': 'Alitas',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/refs/heads/main/OIP%20(1).webp',
+    },
+    {
+      'name': 'Hot Cakes',
+      'image':
+          'https://raw.githubusercontent.com/AngelSPerez/imagenes/81454e69e60c2374c8ba8bee301d266a8cb8e8f0/adadsa.webp',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF8939EF),
+          automaticallyImplyLeading: false,
+          elevation: 2,
+          centerTitle: true,
+          title: const Text(
+            'ReciveRecipe',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
             ),
-          ],
+          ),
+        ),
+        body: SafeArea(
+          child: GridView.builder(
+            padding: const EdgeInsets.all(8),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.85,
+            ),
+            itemCount: _recipes.length,
+            itemBuilder: (context, index) {
+              final recipe = _recipes[index];
+              return _RecipeCard(
+                name: recipe['name']!,
+                imageUrl: recipe['image']!,
+                rating: 4.5,
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _RecipeCard extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+  final double rating;
+
+  const _RecipeCard({
+    required this.name,
+    required this.imageUrl,
+    required this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                imageUrl,
+                width: double.infinity,
+                height: 140,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => Container(
+                  height: 140,
+                  color: Colors.grey.shade300,
+                  child: const Icon(Icons.broken_image, color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Text(
+              name,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 4),
+          _StarRating(rating: rating),
+          const SizedBox(height: 4),
+        ],
+      ),
+    );
+  }
+}
+
+class _StarRating extends StatelessWidget {
+  final double rating;
+
+  const _StarRating({required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(5, (index) {
+        final starValue = index + 1;
+        if (rating >= starValue) {
+          return const Icon(Icons.star, color: Colors.amber, size: 20);
+        } else if (rating >= starValue - 0.5) {
+          return const Icon(Icons.star_half, color: Colors.amber, size: 20);
+        } else {
+          return const Icon(Icons.star_border, color: Colors.amber, size: 20);
+        }
+      }),
     );
   }
 }
